88837a80c76fbca6e0433646bcd95e1b5e1522de
Timothy Grant <timothyleegrant1995@gmail.com>
2025-03-21 23:42:22 -0700

    changed html file to be an ejs file instead to allow for dynamic cchanging of information displayed. Added js file to perform functionality to the webpage. Added an image to be displayed.

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+
 app.get('/', (req,res)=>{
     //res.send("Hello?");
-    res.sendFile(path.join( __dirname, 'standard_page.html'));
+    //res.sendFile(path.join( __dirname, 'standard_page.html'));
+    const data = {title:"My Test Title", name: "Something is here"}
+    res.render('my_sample_html', data);
 });
 
 app.listen(PORT, ()=>{
---
+++ public/js/scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('Java sdkfjeiw');
+    document.querySelector('h1').style.color='green';
+});
